4c7643f01f8d655994f0f7ae6b9ed4581bb783ac
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-10-02 16:59:08 +0300

    added logger functionality and JWT auth

--- src/controllers/auth/signupController.ts
+++ src/controllers/auth/signupController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express"
 import { UsersModel } from "../../models"
 import { encryptPassword } from "../../utils/encryptPassword"
+import { logger } from "../../utils"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
 export const signupController = async (req: Request, res: Response) => {
   const userExists = await UsersModel.findOne({
@@ -8,6 +12,7 @@ export const signupController = async (req: Request, res: Response) => {
       email: req.body.email,
     },
   })
+
   if (userExists) {
     return res.status(400).send({
       type: "error",
@@ -17,10 +22,32 @@ export const signupController = async (req: Request, res: Response) => {
 
   const hashedPassword = encryptPassword(req.body.password)
   try {
-    await UsersModel.create({
+    const user = await UsersModel.create({
       email: req.body.email,
       hashed_password: hashedPassword,
       username: req.body.username,
     })
-  } catch (error) {}
+
+    logger.info(`New user created with email: ${req.body.email}`)
+
+    //generate token at successfull signup so the user doesn't have to manually signin right after
+    const token = jwt.sign(
+      { email: user.email, userId: user.userId },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "12h" }
+    )
+
+    return res.status(200).send({
+      type: "success",
+      message: "User created successfully",
+      token,
+    })
+  } catch (error) {
+    logger.error(`Error creating user: ${error}`)
+
+    return res.status(400).send({
+      type: "error",
+      error: "Error creating user",
+    })
+  }
 }
